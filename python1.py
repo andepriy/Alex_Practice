716b95ac879f9fcc25bbac5964c9032875a8e168
andepriy <mokshadeepak59@gmail.com>
2023-04-07 23:59:34 +0530

    'python1'

---
+++ python1.py
@@ -0,0 +1,28 @@
+#python data types :
+# 1.numbers
+# 2.boolean
+# 3.set
+# 4.dictionery
+# 5.sequence
+# 1.numbers:In numbers will be having int(20),float(1.5),complex(1+3j).
+# Int:python uses the int datatypes to represent whole integer values.
+# ex:0,1,2,3,4,5,6 ....etc.
+# Float:To represent floating point to values (or)decimal values we can use the float datatype.
+# ex:5.6,10.1,1.3...etc.
+# Complex:The combination at real values and imaginary values.
+# ex:2+5j,1+6j.
+# Boolean:boolean is the one of the data type its behave like true(1),false(0).
+# the python boolen type is an one of the bulit-in function.
+# True=1
+# False=0
+# String: String is a sequence of characterstics. It is denoted by single quotations('')&doble quotations(" ").
+# ex:
+a ='swapna'
+print(a)
+# swapna
+b ="bangaram"
+print(b)
+# bangaram
+c = "like's"
+print(c)
+# like's
