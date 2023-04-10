2a9a24af2d92d0070738480db7d94986d2847abb
andepriy <mokshadeepak59@gmail.com>
2023-04-10 21:51:15 +0530

    'Dictionary'

---
+++ Dictionary.py
@@ -0,0 +1,12 @@
+name="1234python developer3456"
+print(''.join([ i for i in name if not i.isdigit()]))
+# ,,,,'pythson developer',,,
+print([i for i in name if not i.isdigit()])
+
+
+# s=[1,2,3,4,5,-6,-7,-8,-9,-3]
+# for i in s:
+    # if i<0:
+    #     print(0)
+    # else:
+    #     print(i)
