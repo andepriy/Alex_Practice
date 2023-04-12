e9ff832449d9a2c29f20ffa6ba7fcb40845b2a39
andepriy <mokshadeepak59@gmail.com>
2023-04-12 22:55:48 +0530

    'practice'

---
+++ practice.py
@@ -0,0 +1,40 @@
+# Operators:
+# 1) Arthematic operators
+
+
+x1=eval(input('Enter 1st input:'))
+x2=eval(input('Enter 2nd input:'))
+
+# addition
+# print('addtiion is:',x1 + x2)
+# Enter 1st input:10
+# Enter 2nd input:12
+# addtiion is: 22
+
+
+# substraction:
+# print('substraction of:',x2 -x1)
+# Enter 1st input:12
+# Enter 2nd input:10
+# substraction of: -2
+
+# print('multiplication:',x1 *x2 +2)
+# Enter 1st input:5
+# Enter 2nd input:10
+# multiplication: 52
+
+
+# print('division:',x2/x1)
+# division: 2.0
+
+# Modulo:
+print('modulo of :', x1%x2)
+# Enter 1st input:50
+# Enter 2nd input:10
+# modulo of : 0
+
+# floor:
+# print('floor:',x1//x2)
+# Enter 1st input:50
+# Enter 2nd input:10
+# floor: 5
